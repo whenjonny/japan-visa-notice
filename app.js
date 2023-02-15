8f9328ab73002c916f508413e0bcbb0914579db5
qiang <billqiang@qq.com>
2023-02-15 14:22:36 +0800

    change into env

--- app.js
+++ app.js
@@ -23,46 +23,47 @@ function init() {
     }, 5000);
 
     setInterval(function() {
-        request.post(
-            'https://hooks.slack.com/services/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',
-            { json: { text: 'try ' + history.length + ' times, no empty slot' }}
-        );
+        sendSlack('try ' + history.length + ' times, no empty slot');
     }, 3600000);
 }
 
 function fetchVisa(url) {
-    //var url = 'https://coubic.com/api/v2/merchants/Embassy-of-Japan/booking_events?renderer=widgetCalendar&start=2023-03-05T00:00:00.000+08:00&end=2023-04-01T23:59:59.999+08:00&resource_public_ids=768296';
-    console.log('fetch visa info start');
-    request(url, function (error, response, body) {
-        const slots = JSON.parse(body);
-        history.push(slots.length);
+    try {
+        //var url = 'https://coubic.com/api/v2/merchants/Embassy-of-Japan/booking_events?renderer=widgetCalendar&start=2023-03-05T00:00:00.000+08:00&end=2023-04-01T23:59:59.999+08:00&resource_public_ids=768296';
+        request(url, function (error, response, body) {
+            const slots = JSON.parse(body);
+            history.push(slots.length);
 
-        if (slots.length > 0 ) {
-            request.post(
-                process.env.SLACK_WEBHOOK_HOST,
-                { json: { text: 'there is ' + slots.length + " slots, \n https://coubic.com/Embassy-of-Japan/widget/calendar/948169?from_pc=month&from_sp=agendaThreeDay" } },
-            );
-        }
-        if (error) {
-            console.log("error:", error);
-            request.post(
-                process.env.SLACK_WEBHOOK_HOST,
-                { json: { text: 'error: ' + error } },
-            );
-        }
-    });
+            if (slots.length > 0 ) {
+                sendSlack(text: 'there is ' + slots.length + " slots, \n https://coubic.com/Embassy-of-Japan/widget/calendar/948169?from_pc=month&from_sp=agendaThreeDay");
+            }
+            if (error) {
+                console.log("error:", error);
+                sendSlack('error: ' + error);
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function sendSlack(msg) {
+    try {
+        request.post(
+            process.env.SLACK_WEBHOOK_HOST,
+            { json: { text: mgs }}
+        );
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 app.get('/', (req, res) => {
   res.send('Hello World!' + JSON.stringify(history))
 })
 
-
 app.get('/webhook', (req, res) => {
-    request.post(
-        process.env.SLACK_WEBHOOK_HOST,
-        { json: { text: 'try ' + history.length + ' times' }}
-    );
+    sendSlack('try ' + history.length + ' times');
     res.send('Hello World!' + JSON.stringify(history))
 })
 
