41423c5686138c418e3508ae8d94d2c08436c0a0
qiang <billqiang@qq.com>
2023-02-15 15:25:57 +0800

    change into env

--- app.js
+++ app.js
@@ -6,28 +6,28 @@ var axios = require('axios');
 const history = [];
 
 try {
-    init();
+    (async function (){
+        while(true){
+            await tick(10000, 'https://coubic.com/api/v2/merchants/Embassy-of-Japan/booking_events?renderer=widgetCalendar&start=2023-03-05T00:00:00.000+08:00&end=2023-04-01T23:59:59.999+08:00&resource_public_ids=948169');
+            await tick(10000, 'https://coubic.com/api/v2/merchants/Embassy-of-Japan/booking_events?renderer=widgetCalendar&start=2023-04-02T00:00:00.000+08:00&end=2023-05-06T23:59:59.999+08:00&resource_public_ids=948169');
+        }
+    })();
 } catch (e) {
     console.log(e);
 }
 
-function init() {
-    setInterval(function() {
-        if (history.length % 2 == 0) {
-            fetchVisa('https://coubic.com/api/v2/merchants/Embassy-of-Japan/booking_events?renderer=widgetCalendar&start=2023-03-05T00:00:00.000+08:00&end=2023-04-01T23:59:59.999+08:00&resource_public_ids=948169');
-        }
-        else if (history.length % 2 != 0) {
-            fetchVisa('https://coubic.com/api/v2/merchants/Embassy-of-Japan/booking_events?renderer=widgetCalendar&start=2023-04-02T00:00:00.000+08:00&end=2023-05-06T23:59:59.999+08:00&resource_public_ids=948169');
-        }
-        console.log('fetch visa info, history: ' + history.length);
-        if (history.length % 10 == 0) {
-            sendSlack('try ' + history.length + ' times, no empty slot');
-        }
-    }, 3000);
+async function tick(timer, url){
+    await fetchVisa(url);
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve();
+        }, timer)
+    })
 }
 
 async function fetchVisa(url) {
     try {
+        console.log("before fetching visa info");
         const response = await axios.get(url);
         const slots = (response.data);
         history.push(slots.length);
@@ -35,6 +35,10 @@ async function fetchVisa(url) {
         if (slots.length > 0 ) {
             sendSlack('there is ' + slots.length + " slots, \n https://coubic.com/Embassy-of-Japan/widget/calendar/948169?from_pc=month&from_sp=agendaThreeDay");
         }
+        if (history.length % 10 == 0) {
+            sendSlack('bot ' + history.length + ' tries.');
+        }
+        console.log("finish fetching visa info, slots: " + slots.length);
     } catch (error) {
         console.error(error);
         sendSlack('error: ' + error);
